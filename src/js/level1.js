b2a5cfcbff670a3025ec154d1fb8eb09c1c55d31
HRJustin <1087405@hr.nl>
2024-07-04 21:29:45 +0200

    Added 2nd player and edited camera follow

--- src/js/level1.js
+++ src/js/level1.js
@@ -1,23 +1,31 @@
 import { Actor, Scene, Vector, Color, BoundingBox, Sound, Timer } from "excalibur";
 import { Resources, ResourceLoader } from './resources.js';
-import { Player } from './player.js'
+import { Player } from './player.js';
 import { Background } from "./background.js";
 
 export class Level1 extends Scene {
-
-    onActivate(){
-        this.background = new Background(Resources.Background.toSprite(), 750, 370, 1.1, 1);
+    onActivate() {
+        this.background = new Background(Resources.Background.toSprite(), 750, 370, 5, 5);
         this.add(this.background);
 
-        this.player = new Player(400, 300, 50, 50, true);
-        this.add(this.player);
+        // Create Player 1 (Red Car)
+        this.player1 = new Player(400, 300, 50, 50, true);
+        this.add(this.player1);
+
+        // Create Player 2 (Blue Car)
+        this.player2 = new Player(500, 300, 50, 50, false);
+        this.add(this.player2);
     }
 
     onPreUpdate(engine, delta) {
         super.onPreUpdate(engine, delta);
-        const playerPos = this.player.pos;
-        this.camera.pos = playerPos; // Camera position (Following)
+
+        // Player 1 camera follow
+        const player1Pos = this.player1.pos;
+        this.camera.pos = player1Pos;
+
+        // Player 2 camera follow
+        // const player2Pos = this.player2.pos;
+        // this.camera.pos = player2Pos;
     }
-
 }
-
--- src/js/player.js
+++ src/js/player.js
@@ -1,43 +1,8 @@
-import { Actor, Vector, Keys, SpriteSheet, CollisionType, Shape } from "excalibur";
-//
-let player1, player2, Resources;
-
-async function initializeSpriteSheets() {
-    const res = await import('./resources.js');
-    Resources = res.Resources;
-
-    player1 = SpriteSheet.fromImageSource({
-        image: Resources.RedCar,
-        grid: {
-            rows: 6,
-            columns: 8,
-            spriteWidth: 187.5,
-            spriteHeight: 250
-        }
-    });
-
-    player2 = SpriteSheet.fromImageSource({
-        image: Resources.BlueCar,
-        grid: {
-            rows: 6,
-            columns: 8,
-            spriteWidth: 187.5,
-            spriteHeight: 250
-        }
-    });
-}
-
-initializeSpriteSheets();
-//
-
-// export let previousScene = {
-//     scene: '',
-// };
-
+import { Actor, Vector, Keys, CollisionType, Shape } from "excalibur";
+import { Resources } from './resources.js';
 
 export class Player extends Actor {
-
-    constructor(x, y, width = 50, height = 50) {
+    constructor(x, y, width = 50, height = 50, isPlayer1 = true) {
         super({
             width: width,
             height: height,
@@ -46,47 +11,59 @@ export class Player extends Actor {
             collisionType: CollisionType.Active
         });
 
-        this.graphics.use(Resources.RedCar.toSprite());
-        // this.pos = new Vector(x, y);
-        // this.vel = new Vector(0, 0);
+        if (isPlayer1) {
+            this.graphics.use(Resources.RedCar.toSprite());
+        } else {
+            this.graphics.use(Resources.BlueCar.toSprite());
+        }
+        
+        this.isPlayer1 = isPlayer1; // Track if this is player 1 or player 2
     }
 
     onPreUpdate(engine) {
         let xspeed = 0;
         let yspeed = 0;
-        if (engine.input.keyboard.isHeld(Keys.W) || engine.input.keyboard.isHeld(Keys.Up)) {
-            yspeed = -200;
+
+        // Player 1 controls
+        if (this.isPlayer1) {
+            if (engine.input.keyboard.isHeld(Keys.W)) {
+                yspeed = -200;
+            }
+            if (engine.input.keyboard.isHeld(Keys.S)) {
+                yspeed = 200;
+            }
+            if (engine.input.keyboard.isHeld(Keys.D)) {
+                xspeed = 200;
+            }
+            if (engine.input.keyboard.isHeld(Keys.A)) {
+                xspeed = -200;
+            }
         }
-        if (engine.input.keyboard.isHeld(Keys.S) || engine.input.keyboard.isHeld(Keys.Down)) {
-            yspeed = 200;
+        // Player 2 controls
+        else {
+            if (engine.input.keyboard.isHeld(Keys.Up)) {
+                yspeed = -200;
+            }
+            if (engine.input.keyboard.isHeld(Keys.Down)) {
+                yspeed = 200;
+            }
+            if (engine.input.keyboard.isHeld(Keys.Right)) {
+                xspeed = 200;
+            }
+            if (engine.input.keyboard.isHeld(Keys.Left)) {
+                xspeed = -200;
+            }
         }
-        if (engine.input.keyboard.isHeld(Keys.D) || engine.input.keyboard.isHeld(Keys.Right)) {
-            xspeed = 200
-        }
-        if (engine.input.keyboard.isHeld(Keys.A) || engine.input.keyboard.isHeld(Keys.Left)) {
-            xspeed = -200
-        }
+
         this.vel = new Vector(xspeed, yspeed);
-        this.graphics.flipHorizontal = (this.vel.x > 0)
+        this.graphics.flipHorizontal = (this.vel.x > 0);
     }
 
     onInitialize(engine) {
         this.on('collisionstart', (event) => this.hitSomething(event));
     }
+
     hitSomething(event) {
-    //     if (event.other instanceof Bridge) {
-    //         if (event.other.identifier === "level1_bridge") {
-    //             this.scene.engine.goToScene('level2');
-    //         } else if (event.other.identifier === "level2_bridge") {
-    //             this.scene.engine.goToScene('level1');
-    //         }
-    //     }
-    //     if (event.other instanceof Enemy) {
-    //         const enemyType = event.other.identifier; // save the enemy in a variable
-    //         this.scene.engine.goToScene('enemyFight');
-    //         this.scene.engine.currentEnemy = event.other; // Track the current enemy
-    //         const enemyFightScene = this.scene.engine.scenes.enemyFight;
-    //         enemyFightScene.updateEnemy(enemyType); // Change the enemy
-    //     }
+        // Collision logic here:
     }
 }
